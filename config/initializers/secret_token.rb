fed38ce137b65e6f5f420d34a056bb4274c77ca7
Elise Worthy <eworthy@bluebox.net>
2012-02-07 20:44:23 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Geolist::Application.config.secret_token = '2ecc201b9d584b67ae29e384f0895e7fa78683a7e4e253033b4d73885c84cb160b5fe1e6b629652db4762d3d013b710c326588ed5e4e18e835f1e1ebf7ae7f4a'
