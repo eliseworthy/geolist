dba0b73fbb1900586cc1a32321931a23fdd1254f
Bookis Smuin <vegan.bookis@gmail.com>
2012-02-13 18:39:50 -0800

    add challenge

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,5 +1,8 @@
 class LocationsController < ApplicationController
-  
+  # A link that goes from index to show that passes a latitude and longitude to show. Display them
+  # Individually on the show page like 
+  # Lat: 1231234
+  # Long: 234234
   def index
   end
   
